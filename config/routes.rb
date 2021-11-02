c4134d1ec0c8e5a068a7b2ec5c6ad8720fd9fda6
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-11-02 10:17:37 -0400

    route for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      namespace :users do
+        resource :user, only: [:create]
+      end
+    end
+  end
 end
