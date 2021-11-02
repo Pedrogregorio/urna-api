4491345f4ef165d273b4783c8f7dcff6a843c625
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-11-02 10:17:47 -0400

    add i18n

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,14 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module UrnaApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
+    I18n.available_locales = ['pt-BR', :en]
+    config.i18n.default_locale = :'pt-BR'
 
     # Configuration for the application, engines, and railties goes here.
     #
