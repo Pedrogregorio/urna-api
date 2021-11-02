e0e3d133fef98dc630174bcadee0d931d6e96515
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-11-02 10:18:32 -0400

    service and controller of user

---
+++ app/services/user/create.rb
@@ -0,0 +1,26 @@
+class User::Create < BusinessProcess::Base
+  needs :user_params
+
+  steps :create_user_on_db
+
+  def call
+    process_steps
+    @user
+  end
+
+  private
+
+  def create_user_on_db
+    @user = User.create(user)
+  end
+
+  def user
+    {
+      name: user_params[:name],
+      age: user_params[:age],
+      voter_registration: user_params[:voter_registration],
+      cpf: user_params[:cpf],
+      voted: false
+    }
+  end
+end
